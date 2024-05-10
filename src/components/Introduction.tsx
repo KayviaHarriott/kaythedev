98f406200a23c08eff8f3c70c9e301ccfbebb3ad
Kayvia Harriott <kharriott@monainformatixltd.com>
2024-05-09 22:18:00 -0500

    Added styling for introduction on mobile devices to be centered

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -17,18 +17,18 @@ export const Introduction = () => {
     <>
       <div className="py-[56px] flex flex-col flex-col-reverse lg:flex-row justify-center items-center mx-auto gap-[32px]">
         <div className="text-white max-w-[450px]">
-          <p>Hi!</p>
-          <div className="text-[32px] flex gap-2">
+          <p className="text-center sm:text-left">Hi!</p>
+          <div className="text-[32px] flex gap-2 text-center items-center justify-center lg:items-start lg:justify-start">
             <p className="font-[800]">I'm </p>
             <p className="font-bold text-[#ED6A59]">Kay Harriott</p>,
           </div>
-          <p className="text-[#DFDFDF] py-6">
+          <p className="text-[#DFDFDF] pt-2 pb-4 text-center lg:text-left">
             Born and raised in Jamaica, I’m a software engineer and developer
             with over 5 years of experience. Driven to providing the best I can,
             I’ll work with you to create and develop the best tech solutions for
             your problems.
           </p>
-          <div className="flex gap-3">
+          <div className="flex gap-3 justify-center lg:justify-start">
             <Button
               variant="contained"
               sx={{
@@ -55,14 +55,14 @@ export const Introduction = () => {
               Contact Me
             </Button>
           </div>
-          <p>
+          <p className="pt-4 text-center lg:text-left">
             (P.S. I built this website with React, TypeScript, TailWindCSS and
             deployed via Netlify!)
           </p>
         </div>
         <div className="flex justify-center items-center">
           <Box sx={{}} className="flex justify-center items-center">
-            <img className="h-[300px]" src={profile_photo} />
+            <img className="h-[200px] lg:h-[300px]" src={profile_photo} />
           </Box>
         </div>
       </div>
