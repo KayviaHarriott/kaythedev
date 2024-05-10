ddb33323669470501f64d897d64b447026ad70d0
Kayvia Harriott <kharriott@monainformatixltd.com>
2024-05-09 23:21:10 -0500

    Added netlify tag to process forms

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -60,7 +60,7 @@ export const Contact = () => {
           </div>
         </div>
         <div className="flex justify-center items-center w-full pt-6 lg:pt-0">
-          <form className="flex flex-col bg-[#12688D] p-3 rounded-lg">
+          <form name="contact" className="flex flex-col bg-[#12688D] p-3 rounded-lg" netlify>
             {/* <form > //action="/action_page.php"> */}
             <p className="text-white pb-2">Send me a message</p>
             <div className="flex flex-col gap-2 pb-4">
