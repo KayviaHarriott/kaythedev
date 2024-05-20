f92e011a519f358280b462a2200c7483a2581e36
Kayvia Harriott <kharriott@monainformatixltd.com>
2024-05-20 13:59:15 -0500

    Added site map and removed unused code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
-// import '@fontsource/press-start-2p';
-// import '@fontsource/ibm-plex-sans-kr';
-// import '@fontsource-variable/inter'
-// import "@fontsource/poppins";
-// import { TicketInformation } from "./pages/TicketsInformation";
 
 export default function App() {
   return (
@@ -14,10 +9,7 @@ export default function App() {
         <Route path="/">
           <Route index element={<LandingPage />} />
         </Route>
-        {/* <Route path="/ticket-information" index element={<TicketInformation />} /> */}
       </Routes>
     </BrowserRouter>
   );
 }
-
-// ReactDom.render(<App />, document.getElementById("root"));
