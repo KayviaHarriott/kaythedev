1187607466b164c1c2c2e2cfc4fa4a7fab82f863
Kayvia Harriott <kharriott@monainformatixltd.com>
2024-05-20 14:26:19 -0500

    Added meta tags to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,6 +9,7 @@ import { TechStack } from "../components/TechStack";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 import { CustomCarousel } from "../components/CustomCarousel";
+import { Helmet } from "react-helmet";
 
 export const LandingPage = () => {
   useEffect(() => {
@@ -16,10 +17,46 @@ export const LandingPage = () => {
   }, []);
 
   const styling = "max-w-[1200px] mx-auto px-[32px]";
-  
-  
+
   return (
     <>
+      {/* Meta Tags */}
+      <Helmet>
+        <title>ktd | Kayvia Harriott</title>
+        <meta
+          name="description"
+          content="Kay Harriott, a freelance web developer and software engineer. Born and raised in Jamaica, I’m a software engineer and developer
+            with over 5 years of experience. Driven to providing the best I can,
+            I’ll work with you to create and develop the best tech solutions for
+            your problems."
+        />
+        <meta
+          name="keywords"
+          content={`React, TypeScript, developer, portfolio, kayvia harriott, kayvia, harriott, 
+            web development, freelance web dev, make website, create website, wordpress, elementor freelance, 
+            elementor, software developer, front-end developer, fullstack developer, frontend developer`}
+        />
+        <meta name="author" content="Kayvia Harriott" />
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Kayvia Harriott",
+              "url": "https://kaythedev.com",
+              "sameAs": [
+                "https://www.linkedin.com/in/kayviaharriott",
+                "https://github.com/KayviaHarriott"
+              ],
+              "jobTitle": "Software Developer",
+              "worksFor": {
+                "@type": "Organization",
+                "name": "KayTheDev"
+              }
+            }
+          `}
+        </script>
+      </Helmet>
       {/* Introduction */}
       <div className="bg-[#1B2845]">
         <NavigationBar />
